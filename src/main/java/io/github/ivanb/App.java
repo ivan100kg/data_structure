2b31a4f07ca911059a674962077a8ae4c5e44106
ivanb <ivar110kg@gmail.com>
2025-02-17 16:30:12 +0400

    Add list

--- src/main/java/io/github/ivanb/App.java
+++ src/main/java/io/github/ivanb/App.java
@@ -1,10 +1,11 @@
 package io.github.ivanb;
 
 import io.github.ivanb.struct.Array;
+import io.github.ivanb.struct.List;
 
 public class App {
     public static void main(String[] args) {
-        Struct struct = new Array();
+        Struct struct = new List();
         struct.show();
     }
 }
